05321d01a2da1ce6f248b2bb9483574f077096e4
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-09-08 12:12:22 +0300

    feat: написать роут и контроллер для user, добавить ошибки

---
+++ controllers/users.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const BadRequest = require('../errors/badRequest');
+const DuplicateKeyError = require('../errors/duplicateKeyError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports.createUser = (req, res, next) => {
+  const { email, name, password } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({ email, name, password: hash }))
+    .then((data) => {
+      res.status(200).send({
+        email: data.email,
+        name: data.name,
+        _id: data._id,
+      });
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new BadRequest('Не верные данные'));
+      } else if (error.code === 11000) {
+        next(new DuplicateKeyError('Пользователь с указнным email существует'));
+      } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.getUserInfo = (req, res, next) => {
+  const userId = req.user._id;
+  return User.findById(userId)
+    .then((data) => {
+      res.status(200).send({
+        email: data.email,
+        name: data.name,
+      });
+    })
+    .catch(next);
+};
+
+module.exports.updateUserInfo = (req, res, next) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new BadRequest('Некоректные данные'));
+      } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
+      res.status(200).send({ token });
+    })
+    .catch(next);
+};
