1d5a7b934a07ef1535bd7ce7d60c36935b15de60
Vladislav Evdokimov <gangrena14@yandex.ru>
2023-01-16 22:28:35 +0300

    feat: изменить отправляемые данные

--- controllers/users.js
+++ controllers/users.js
@@ -35,8 +35,8 @@ module.exports.getUserInfo = (req, res, next) => {
     .then((data) => {
       res.status(200).send({
         email: data.email,
-        _id: data._id,
         name: data.name,
+        _id: data._id,
       });
     })
     .catch(next);
